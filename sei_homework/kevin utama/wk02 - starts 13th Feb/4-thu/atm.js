cfbd0989ded43fe04392fdadcd861581cdd7e9b3
utamakevin <utama.kevin@yahoo.com>
2023-02-16 19:43:26 +1100

    thurs homework

---
+++ sei_homework/kevin utama/wk02 - starts 13th Feb/4-thu/atm.js
@@ -0,0 +1,102 @@
+const btn1 = document.querySelector("#btn1")
+const btn2 = document.querySelector("#btn2")
+const btn3 = document.querySelector("#btn3")
+const btn4 = document.querySelector("#btn4")
+const btn5 = document.querySelector("#btn5")
+const btn6 = document.querySelector("#btn6")
+const btn7 = document.querySelector("#btn7")
+const btn8 = document.querySelector("#btn8")
+const btn9 = document.querySelector("#btn9")
+const btn0 = document.querySelector("#btn0")
+const btnClr = document.querySelector("#btnClr")
+const btnEnter = document.querySelector("#btnEnter")
+
+const deposit = document.querySelector(".deposit")
+const withdraw = document.querySelector(".withdraw")
+
+const display = document.querySelector("#display")
+const balance = document.querySelector(".balance")
+
+const moreInfo = document.querySelector(".more-info")
+
+
+
+console.log(btn1)
+
+
+
+
+
+const num1Input = document.querySelector(".num1-input")
+const num2Input = document.querySelector(".num2-input")
+const p = document.querySelector("p")
+
+function handleInput(e) {
+
+  console.log(`button ${e.target.id} pressed`)
+  display.value += e.target.value
+}
+
+function handleClear(e) {
+    display.value = ""
+}
+
+// function handleEnter(e) {
+//     balance.value += display.value
+//     handleClear
+//     return display.value
+// }
+
+function handleDeposit(e) {
+    let result = Number(balance.textContent) + Number(display.value)
+    balance.textContent = result
+    
+
+    if(balance.textContent !== 0) {
+    document.body.style.backgroundColor = "white"
+    }
+
+    display.value = ""
+}
+
+function handleWithdraw(e) {
+    
+    let result = Number(balance.textContent) - Number(display.value)
+    
+    if(result > 0) {
+        balance.textContent = result
+    } else if(result == 0) {
+        balance.textContent = result
+        document.body.style.backgroundColor = "red"
+        moreInfo.style.color = "white"
+        moreInfo.textContent = "You are broke."
+    } else {
+        insufficientFunds()
+    }
+    display.value = ""
+}
+
+function insufficientFunds() {
+    moreInfo.textContent = "Insufficient funds."
+    document.body.style.backgroundColor = "mistyrose"
+    moreInfo.style.color = "black"
+}
+
+
+btn1.addEventListener("click", handleInput)
+btn2.addEventListener("click", handleInput)
+btn3.addEventListener("click", handleInput)
+btn4.addEventListener("click", handleInput)
+btn5.addEventListener("click", handleInput)
+btn6.addEventListener("click", handleInput)
+btn7.addEventListener("click", handleInput)
+btn8.addEventListener("click", handleInput)
+btn9.addEventListener("click", handleInput)
+btn0.addEventListener("click", handleInput)
+btnClr.addEventListener("click", handleClear)
+// btnEnter.addEventListener("click", handleEnter)
+
+deposit.addEventListener("click", handleDeposit)
+withdraw.addEventListener("click", handleWithdraw)
+
+
