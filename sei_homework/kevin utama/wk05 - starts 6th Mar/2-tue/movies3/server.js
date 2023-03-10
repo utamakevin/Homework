fd8915a0e4a451977a4e317ea53892634e926a0e
utamakevin <utama.kevin@yahoo.com>
2023-03-10 23:22:52 +0700

    movie3 update

--- sei_homework/kevin utama/wk05 - starts 6th Mar/2-tue/movies3/server.js
+++ sei_homework/kevin utama/wk05 - starts 6th Mar/2-tue/movies3/server.js
@@ -20,40 +20,58 @@ app.get('/', (req, res) => {
 app.get('/search', (req, res) => {
     omdb.findManyByTitle(req.query.title, (err, movieList) => {
         let movieTitleListArray = []
-        // let imdbidListArray = []
+        let imdbidListArray = []
         for(movie of movieList.Search) {
-            // imdbidListArray.push(movie.imdbID)
+            console.log(movie.imdbID)
+            
+            imdbidListArray.push(movie.imdbID)
             movieTitleListArray.push(movie.Title)
         }
         // console.log(imdbidListArray)
         // console.log(movieTitleListArray)
-        res.render('search', { movieTitleListArray })
+        res.render('search', { movieTitleListArray, imdbidListArray })
     }
     )
 })
 
-app.get('/check_db/:title', (req, res) => {
-    const sql = `SELECT title FROM movies WHERE title ='${req.params.id}'`
+app.get('/check_db/:id', (req, res) => {
+    const sql = `SELECT * FROM movies WHERE imdbid ='${req.params.id}'`
     pool.query(sql, (err, dbRes) => {
+        console.log(dbRes.rows)
+        console.log(dbRes.rowCount)
         if(dbRes.rowCount !== 0) {
-            res.send(dbRes.rows[0].title)
+            // res.send(dbRes.rows[0].title)
+            res.redirect(`/localLibrary/${req.params.id}`)
         } else {
-            res.redirect(`/movie?title=${req.params.title}`)
+            res.redirect(`/movie/${req.params.id}`)
         }
     })
 })
 
-app.get('/movie', (req, res) => {
-    omdb.findOneByTitle(req.query.title, (err, movie) => {
-        
-        // save title, year, poster
-        const sql = `INSERT INTO movies (imdbid, title, release_year, poster_url) VALUES ('${movie.imdbID}', '${movie.Title}', '${movie.Year}', '${movie.Poster}');`
+app.get('/localLibrary/:id', (req, res) => {
+    const sql = `SELECT * FROM movies WHERE imdbid = $1`
+    pool.query(sql, [req.params.id], (err, dbRes) => {
+        console.log(err)
+        const movieTitle = dbRes.rows[0].title
+        const movieYear = dbRes.rows[0].release_year
+        const moviePoster = dbRes.rows[0].poster_url
+        res.render('localLibrary', { movieTitle, movieYear, moviePoster })
+    })
+})
 
-        pool.query(sql, (err, dbRes) => {
-            console.log(err)
-            res.render('movie', {movieTitle: movie.Title, movieYear: movie.Year , moviePoster: movie.Poster})
+app.get('/movie/:id', (req, res) => {
+
+    omdb.findOneById(req.params.id, (err, movie) => {
+        console.log(err)
+        console.log(movie)
+        const sql = `INSERT INTO movies (imdbid, title, release_year, poster_url) VALUES ($1, $2, $3, $4);`
+        pool.query(sql, [movie.imdbID, movie.Title, movie.Year, movie.Poster], (err, dbRes) => {
+            const movieTitle = movie.Title
+            const movieYear = movie.Year
+            const moviePoster = movie.Poster
+            res.render('movie', { movieTitle, movieYear, moviePoster })
         })
-
+        
     })
 })
 
