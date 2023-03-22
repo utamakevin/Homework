a1068671ae6ec69a44d951c40d10f0963a8deeec
utamakevin <utama.kevin@yahoo.com>
2023-03-23 02:33:51 +0700

    revision

--- sei_homework/kevin utama/wk07 - starts 20th Mar/4-thu/monkey_class/monkey.js
+++ sei_homework/kevin utama/wk07 - starts 20th Mar/4-thu/monkey_class/monkey.js
@@ -5,12 +5,13 @@ class Monkey {
         this.foodsEaten = []
     }
 
+    // public instance methods
     eat(food) {
         this.foodsEaten.push(food)
     }
 
     introduce() {
-        return `Hi my name is ${this.name}. I am a ${this.species}. I had ${this.foodsEaten} for brunch.`
+        return `Hi my name is ${this.name}. I am a ${this.species}. I had ${this.foodsEaten.join(', ')} for brunch.`
     }
 
 }
