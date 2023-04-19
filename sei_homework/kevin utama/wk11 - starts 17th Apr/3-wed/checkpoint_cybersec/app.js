635e7b3e1f6de259e9b0e2a6732ba47ef6571bd1
utamakevin <utama.kevin@yahoo.com>
2023-04-19 13:19:36 +0700

    checkpoint

---
+++ sei_homework/kevin utama/wk11 - starts 17th Apr/3-wed/checkpoint_cybersec/app.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+const util = require('util')
+
+const data = JSON.stringify(fs.readFileSync('./data.txt', 'utf8'))
+
+const unit = data
+    .slice(1, data.length - 1)
+    .split('\\n')
+    .slice(1)
+    .filter(elem => elem.trim() !== '')
+
+
+let result = {}
+
+for (i = 0; i < unit.length; i++) {
+    result[unit[i].split(',')[0]] = []
+}
+
+for (i = 0; i < unit.length; i++) {
+    result[unit[i].split(',')[0]] += {
+        product: unit[i].split(',')[1],
+        unitPrice: unit[i].split(',')[2],
+        units: unit[i].split(',')[3],
+        totalPrice: unit[i].split(',')[2] * unit[i].split(',')[3]
+    }
+}
+
+
+console.log(result)
+
+
+module.exports = result
---
+++ sei_homework/kevin utama/wk11 - starts 17th Apr/3-wed/checkpoint_cybersec/app.test.js
@@ -0,0 +1,15 @@
+const { exportAllDeclaration } = require('@babel/types');
+const result = require('./app.js');
+
+describe('Main', () => {
+  test('Alicia Brereton has one type of contraband', () => {
+    exportAllDeclaration(result['Alicia Brereton']).toBe([
+        {
+          product: 'Marijuana',
+          unitPrice: '9.18',
+          units: '50',
+          totalPrice: '459.00'
+        }
+      ])
+  })
+})
